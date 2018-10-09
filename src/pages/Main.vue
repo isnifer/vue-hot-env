6ef434facd04ea10e15ec9896ee5657f6f088cde
Anton Kuznetsov <isnifer@gmail.com>
2018-10-10 02:40:14 +0300

    Fix after rebase

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -11,27 +11,6 @@ import { mapActions, mapState } from 'vuex'
 
 export default {
   name: 'Main',
-<<<<<<< HEAD
-  data() {
-    return {
-      posts: [
-        { id: 1, name: 'Item 1' },
-        { id: 2, name: 'Item 2' },
-        { id: 3, name: 'Item 3' },
-        { id: 4, name: 'Item 4' },
-        { id: 5, name: 'Item 5' },
-      ]
-    }
-  },
-  created() {
-    console.log(`Hello ${this.name}`)
-  },
-  methods: {
-    handleItemClick({ id, name }) {
-      console.log({ id, name })
-    }
-  },
-=======
 
   data() {
     return {
@@ -54,7 +33,6 @@ export default {
       'posts',
     ]),
   }
->>>>>>> Add load_posts
 }
 </script>
 
